c3416aab20e9b66c8d113550e962382faa2bb813
datomaluta <datomaluta@gmail.com>
2022-09-04 15:43:27 +0400

    clean some css code in project

--- src/helpers/LocalStorageFunctions.js
+++ src/helpers/LocalStorageFunctions.js
@@ -1,22 +1,3 @@
-// export const clearLocalStorage = () => {
-//   localStorage.removeItem("enteredName");
-//   localStorage.removeItem("enteredSurname");
-//   localStorage.removeItem("team");
-//   localStorage.removeItem("position");
-//   localStorage.removeItem("email");
-//   localStorage.removeItem("phoneNumber");
-//   localStorage.removeItem("laptopName");
-//   localStorage.removeItem("laptopBrand");
-//   localStorage.removeItem("cpu");
-//   localStorage.removeItem("cpuCores");
-//   localStorage.removeItem("cpuThreads");
-//   localStorage.removeItem("ram");
-//   localStorage.removeItem("memoryType");
-//   localStorage.removeItem("purchaseDate");
-//   localStorage.removeItem("price");
-//   localStorage.removeItem("state");
-// };
-
 export const clearLocalStorage = () => {
   localStorage.removeItem("userData");
   localStorage.removeItem("laptopData");
